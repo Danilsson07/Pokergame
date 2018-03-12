179bce964d61e572ef6931089a8977fa5d6829a7
Txqii <timothy.triebold@students.fhnw.ch>
2018-03-12 17:25:48 +0100

    Deck

---
+++ src/package/Model/Deck.java
@@ -0,0 +1,43 @@
+package Model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import Model.Suit;
+
+
+public class Deck {
+    private final ArrayList<Card> allCards;
+
+    public Deck() {
+        this.allCards = new ArrayList<>();
+        shuffle();
+    }
+
+    public int getCardsleft() {
+        return allCards.size();
+    }
+
+    public void shuffle() {
+        allCards.clear();
+
+        for (Card.Suit suit : Card.Suit.values()) {
+            for (Card.Rank rank : Card.Rank.values()) {
+                Card card = new Card(suit, rank);
+                allCards.add(card);
+            }
+        }
+
+        Collections.shuffle(allCards);
+    }
+
+    public Card dealCard() {
+        if (allCards.size() > 0) {
+            Card x = allCards.get(allCards.size()-1);
+            allCards.remove(allCards.size()-1);
+            return x;
+        } else {
+            return null;
+        }
+    }
+}
+
